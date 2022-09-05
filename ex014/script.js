baf8309a27cacbbb1c4586289b3b7d10973173ef
joaoedson62 <joaoedsonm63@gmail.com>
2022-09-04 22:40:43 -0300

    criado o exercicio

---
+++ ex014/script.js
@@ -0,0 +1,11 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if (ini.ariaValueMax.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam Dados' )
+    } else {
+        Tudo OK!
+    }
+}
